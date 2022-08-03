4ddbfa5e60505906ccd6dd53c99a2a545b9500f5
Matheus Santos <matheussantos77@outlook.com>
2022-08-03 17:50:22 -0300

    Cadastrando Empresa

--- src/Pages/Customers/index.js
+++ src/Pages/Customers/index.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
+import firebase from "../../Services/firebaseConnection";
 import './styles.css'
 
+import { toast } from 'react-toastify'
 import { AiOutlineUser } from 'react-icons/ai'
 
 import Title from "../../Components/Title";
 import Header from "../../Components/Header";
 
 function Customers() {
+
+    const nameRef = useRef()
+    const cnpjRef = useRef()
+    const andressRef = useRef()
+
+    async function handleAdd(e) {
+        const name = nameRef.current.value
+        const cnpj = cnpjRef.current.value
+        const andress = andressRef.current.value
+
+        e.preventDefault()
+
+        if(name !== '' && cnpj !== '' && andress !== '') {
+            await firebase.firestore().collection('customers').add({
+                NomeDaEmpresa: name,
+                CNPJ: cnpj,
+                Endereço: andress
+            })
+    
+            .then(() => {
+                toast.success("Empresa cadastrada com sucesso!")
+            })
+    
+            .catch(error => {
+                toast.error("Ops, algo deu errado!")
+                console.log(error)
+            })
+        } else {
+            toast.info("Preencha todos os campos!")
+        }
+    }
+
+
     return (
         <div className="container">
             <Header />
@@ -18,20 +53,20 @@ function Customers() {
                 </Title>
 
                 <div className="info-customers">
-                    <form className="form-customers">
+                    <form onSubmit={handleAdd} className="form-customers">
                         <label>
                             Nome Fantasia
-                            <input placeholder="Nome da sua empresa" />
+                            <input placeholder="Nome da sua empresa" ref={nameRef}/>
                         </label>
 
                         <label>
                             CNPJ
-                            <input placeholder="Seu CNPJ" />
+                            <input placeholder="Seu CNPJ" ref={cnpjRef}/>
                         </label>
 
                         <label>
                             Endereço
-                            <input placeholder="Seu endereço" />
+                            <input className="textCaptalize" placeholder="Seu endereço" ref={andressRef}/>
                         </label>
 
                         <button>
