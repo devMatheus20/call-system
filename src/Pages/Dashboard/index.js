1b5e5f5b198333674bc44325cf13f0218a57cc09
Matheus Santos <matheussantos77@outlook.com>
2022-08-15 17:17:50 -0300

    Criando tabela dos chamados

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
 
-import { BiMessageDots } from 'react-icons/bi'
+import { BiMessageDots, BiSearch, BiEditAlt } from 'react-icons/bi'
 import { HiPlusSm } from 'react-icons/hi'
 
 import Title from '../../Components/Title'
@@ -19,7 +19,7 @@ function Dashboard() {
             <div className='calleds'>
                 <Title>
                     <BiMessageDots color="#000" size={27} />
-                    Atendimentos
+                    Chamados
                 </Title>
 
                 {calleds.length === 0 ?
@@ -38,9 +38,37 @@ function Dashboard() {
                         <HiPlusSm size={25} color="#fff" />
                         Novo chamado
                     </Link>
-
-
                 }
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Cliente</th>
+                            <th scope='col'>Assunto</th>
+                            <th scope='col'>Status</th>
+                            <th scope='col'>Cadastrado em</th>
+                            <th scope='col'>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-label="Cliente">Sujeito</td>
+                            <td data-label="Assunto">Suporte</td>
+                            <td data-label="Status">
+                                <span className='stats'>Em aberto</span>
+                            </td>
+                            <td data-label="Cadastrado em">02/05/2022</td>
+                            <td data-label="#" className='actions'>
+                                <button className='search'>
+                                    <BiSearch color='#fff' size={20} />
+                                </button>
+                                <button className='edit'>
+                                    <BiEditAlt color='#fff' size={20} />
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
 
         </div>
